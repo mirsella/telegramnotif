0945b00c776a9426e26135b9301c1eb27c775832
mirsella <mirsella@protonmail.com>
2021-06-05 00:26:50 +0200

    added a options for http query parameters

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 see https://github.com/mirsella/telegram-notif.sh for instructions on how to create a bot and get the id / token
+
+how to use it : 
+look into test.js
+
+the 4th argument (options) is a object of query parameters to add to the http request
--- test.js
+++ test.js
@@ -8,6 +8,6 @@ eval(fs.readFileSync('/home/mirsella/.config/token/telegram.token') + '')
 telegramnotif(TgId, TgToken, `
 npm run test
 ${new Date()}
-yep
-`)
+<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">yup</a>
+`, { parse_mode: 'HTML'})
     .then(e => console.log(e))
